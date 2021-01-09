ccccfca7d6f57f2fb0a8d103451ad6e07290fe7d
Pablo Rivero <pablo.rivero@eden.clinic>
2021-01-10 00:26:27 +0100

    :art: Header

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from './components/Header/Header';
 import NotFoundPage from './containers/NotFoundPage';
 import Home from './containers/Home/Home';
 import PetPage from './containers/PetPage/PetPage';
@@ -7,6 +8,7 @@ import PetPage from './containers/PetPage/PetPage';
 const App = () => (
   <BrowserRouter>
     <div className="App" data-testid="main-app">
+      <Header />
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/pet/:id" component={PetPage} />
---
+++ src/app/components/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+const Header = () => (
+  <div>
+    <Navbar>
+      <NavbarBrand href="/">Wofder!</NavbarBrand>
+    </Navbar>
+  </div>
+);
+
+export default Header;
