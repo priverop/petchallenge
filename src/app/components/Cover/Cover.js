0707ecfc07d30809186b74f7d3c4fc813485c43c
Pablo Rivero <pablo.rivero@eden.clinic>
2021-01-11 03:16:32 +0100

    :art: Cover styles + link smooth

--- src/app/components/Cover/Cover.js
+++ src/app/components/Cover/Cover.js
@@ -1,17 +1,32 @@
 import { Row, Col, Container, Button } from 'reactstrap';
 import './Cover.scss';
+import Pulse from 'react-reveal/Pulse';
+import RubberBand from 'react-reveal/RubberBand';
+import smoothScrolling from '../../utils/smoothScrolling';
 
 const Cover = () => (
   <Row id="Cover">
     <Container>
       <Row>
         <Col>
-          <h1 className="mb-3">
-            Find your <span className="main-color">perfect</span> partner today
-          </h1>
-          <Button className="colorful-button" size="lg">
-            Let&apos;s go!
-          </Button>
+          <Pulse>
+            <h1 className="mb-3 main-color-text">
+              Find your <span className="main-color">perfect</span> partner
+              today
+            </h1>
+          </Pulse>
+
+          <RubberBand delay={1000}>
+            <Button
+              className="colorful-button"
+              size="lg"
+              onClick={() => {
+                smoothScrolling('PetGallery');
+              }}
+            >
+              Let&apos;s go!
+            </Button>
+          </RubberBand>
         </Col>
       </Row>
     </Container>
---
+++ src/app/utils/smoothScrolling.js
@@ -0,0 +1,4 @@
+const smoothScrolling = id =>
+  document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+
+export default smoothScrolling;
